a065935dbea24897fbcc9e6026f5fd761b205de5
Katlyn Gabor <katlyngabor@gmail.com>
2014-07-22 08:52:56 -0400

    added new route

--- scripts/main.js
+++ scripts/main.js
@@ -18,6 +18,7 @@ var Posts = Backbone.Collection.extend ({
 
 });
 
+// var all_posts = new Posts();
 
 
 // var Student = Backbone.Model.extend ({
@@ -38,13 +39,12 @@ var Posts = Backbone.Collection.extend ({
 // });
  
 
-
 var PublishedView = Backbone.View.extend({
 		
 		el:'.wrapper',
 
-	 	events: {
-     	'click .submit' : 'postBlog'
+	 	events: { 
+     	'click a' : 'viewPost'
  		},
 
 		initialize: function(){
@@ -60,58 +60,74 @@ var PublishedView = Backbone.View.extend({
 			$('.published-container').html(rendered);
 		},
 
-		postBlog: function(event){
-			event.preventDefault();
-			console.log('the button is buttoning!');
-
-			// Grab all form data and define variables for each to use below and create new instance of your model 
-    	var temp_post = new Post({
-	      title:  $('.title-container').val(),
-	      content: $('.create-post-container').val(),
-	      author:  $('.author-container').val(),
-	      // tags: $('.tags').val(),
-	      // tags: tags.replace(/\s+/g, '').split(','),
-	      status: 'Published',
-	      // date: new Date().toJSON().slice(0,10)
-
-	    });
+		// postBlog: function(event){
+		// 	event.preventDefault();
+		// 	console.log('the button is buttoning!');
   
-    // Save your model; this will save it to the database and re-render the page
-    	all_posts.add(temp_post).save();
+  //   // Save your model; this will save it to the database and re-render the page
+  //   all_posts.add(temp_post).save();
 
-    	$("form")[0].reset();
+  //   	$("form")[0].reset();
 
-   }
+  //   },
+
+
+    viewPost: function (event){
+    	console.log('you got here');
+  		event.preventDefault();
+  		event.stopPropagation();
+  		var postid = $(event.target).attr('id');
+  		window.blog_router.navigate('#post/'+postid, {trigger: true});
+  		$('.published-container').hide();
+  		$('.singleViewContainer').show();
+  	}
+
 
 });
 
 		
+var SingleView = Backbone.View.extend({
+		
+		el:'.singleViewContainer',
 
-		// events: {
-  //   'click button#openEssay':'openEssay'
+	 	events: {
+     	'click .return' : 'returnHome'
+     	// 'click .deletePostBtn' : 'deletePost'
+ 		},
+
+		initialize: function(attributes){
+  	this.singlePost = this.collection.get(attributes.postid);
+  	this.render();
+		},
+
+	  render: function (options){
+  	var template = Handlebars.compile($('#singleViewTemplate').html());
+		var rendered = template(this.singlePost.toJSON());
+    this.$el.html(rendered);
+  	},
+
+		returnHome: function (event){
+			event.preventDefault();
+		  window.blog_router.navigate('', { trigger: true });
+		  $('.published-container').show();
+		  $('.singleViewContainer').hide();
+
+   	},
+
+  //  	 deletePost: function (event) {
+  //   	event.preventDefault();
+  //   	event.stopPropagation();
+  //   	// Confirmation dialogue
+  //   	if (window.confirm("Are you sure you want to delete this post?")) {
+  //     	this.singlePost.destroy({success: function () {
+  //       window.blog_router.navigate('', { trigger: true });
+  //     }});
+  //   }
   // }
 
+});
 
-
-// var StudentView = Backbone.View.extend({
- 
-//   template: function(model){
-//     return _.template($('#student_list').html());
-//   },
- 
-//   el: $('.hero-unit ul'),
- 
-//   initialize: function(){
-//     this.render();
-//     this.collection.on('change', this.render, this);
-//     this.collection.on('destroy', this.render, this);
-//   },
- 
-//   render: function(){
-//     this.$el.html( this.template(this.collection) )
-//   }
- 
-// });
+		
 // Create an instance of my Collection
 var all_posts = new Posts();
 
@@ -119,7 +135,8 @@ var all_posts = new Posts();
 // Grab all my data from my server
 // After it's complete, create a new view with data
 all_posts.fetch().done(function () {
-  new PublishedView( { collection: all_posts });
+  window.blog_router = new BlogRouter();
+	Backbone.history.start();
 });
  
  
@@ -130,4 +147,77 @@ $("button").on("click", function() {
   $("body").toggleClass("dialogIsOpen");
 
 });
-	
+
+
+$('.modal').on('submit', function (event) {
+  event.preventDefault();
+
+  var temp_post = new Post({
+    title: $('.title-container').val(),
+    author: $('.author-container').val(),
+    content: $('.create-post-container').val()
+  });
+
+  all_posts.add(temp_post).save();
+
+  // $(this).trigger('reset');
+  
+});
+
+
+
+// // 	// Create an instance of my Collection
+// // var whiskey_list = new WhiskeyCollection();
+
+// // Grab all my data from my server
+// // After it's complete, create a new view with data
+// whiskey_list.fetch().done( function (){
+//   // Define Global Router && Start History
+//   window.whiskey_router = new WhiskeyRouter();
+//   Backbone.history.start();
+// });
+
+
+// Submit Form
+// Create a new Whiskey and add it to my collection
+// Interesting I put this here - http://goo.gl/2RbmvE
+// but I did so because it didn't make sense (to me anyway) to have it as a view
+// $('#newWhiskey').on('submit', function (event) {
+
+//   event.preventDefault();
+
+//   // Creates an instance (entry in my DB) of a whiskey model
+//   var temp_whiskey = new Whiskey({
+//     name: $('.whiskey_title').val(),
+//     description: $('.whiskey_desc').val()
+//   });
+
+//   // Adds said whiskey to my collection - my liquor cabinet
+//   whiskey_list.add(temp_whiskey).save();
+
+//   // Resets my form - skadoosh
+//   $(this).trigger('reset');
+
+// This is my router. It will react to the URL I visit and run a function based on that
+// Right now I only have 2, but I could easily add a lot more.
+// Also, both trigger a new view instance currently
+var BlogRouter = Backbone.Router.extend({
+
+  routes: {
+    '' : 'home',
+    'post/:id' : 'singleView'
+  },
+
+  home: function() {
+    new  PublishedView( { collection: all_posts });
+  },
+
+
+  singleView: function(id) {
+    new SingleView({ postid: id, collection: all_posts });
+  }
+
+});
+
+// id in 'post/:id' : 'singleView' is the same as id in singleView: function(id)
+
